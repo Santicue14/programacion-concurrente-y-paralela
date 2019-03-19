57d162b334b40307cab750d5168ace0dfb41c86c
Victor Contreras <vcontreras@gmail.com>
2019-03-19 19:33:29 -0300

    Add Lisat

--- Solution-Collections/ConsoleAppUI/Program.cs
+++ Solution-Collections/ConsoleAppUI/Program.cs
@@ -8,8 +8,19 @@ namespace ConsoleAppUI
 {
     class Program
     {
+        private static string[] colors = { "MAGENTA", "RED", "WHITE", "BLUE", "CYAN" };       
+        private static string[] removeColors = { "RED", "WHITE", "BLUE" };
+
+
+
         static void Main(string[] args)
         {
+            
+            foreach (var item in colors)
+            {
+                Console.WriteLine("Color: {0}", item);
+            }
+            Console.ReadKey();
         }
     }
 }
